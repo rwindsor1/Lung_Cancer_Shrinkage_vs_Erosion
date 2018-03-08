6de5f4d250405b711e76dc28d74661aa24046d7e
rwindsor1 <rhydianwindsor@aol.co.uk>
2018-03-08 14:44:51 +0000

    Fixed bug in create_mask

--- python_scripts/mphys_project_functions.py
+++ python_scripts/mphys_project_functions.py
@@ -13,7 +13,7 @@ def create_mask(gtv_arr, threshold = 500, radius_of_mask = 2):
 	mask_arr = gtv_arr
 	mask_arr[np.where(mask_arr<threshold)] = 0
 	for i in range(np.shape(mask_arr)[2]):
-		scan_slice = combinedData[:,:,i].binary_fill_holes(scan_slice)
+		scan_slice = mask_arr[:,:,i].binary_fill_holes(scan_slice)
 		scan_slice = skimage.morphology.remove_small_objects(scan_slice.astype(bool), min_size=20, connectivity=1, in_place=False)
 		edges = feature.canny(scan_slice,sigma=2)
 		mask = skimage.filters.rank.maximum(edges,skimage.morphology.disk(radius_of_mask))
