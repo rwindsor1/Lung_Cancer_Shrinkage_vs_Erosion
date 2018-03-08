903bcabcc7db6000ee10ffef164523ac1e2939a6
rwindsor1 <rhydianwindsor@aol.co.uk>
2018-03-08 15:48:57 +0000

    Added healthy tissue funtion

--- python_scripts/mphys_project_functions.py
+++ python_scripts/mphys_project_functions.py
@@ -7,18 +7,26 @@ from skimage import feature
 import skimage
 
 # generates a mask array from a numpy array
-def create_mask(gtv_arr, threshold = 500, radius_of_mask = 2):
+def create_mask(gtv_arr, threshold = 500,sigma = 0.05, radius_of_mask = 2,small_obj_size = 20):
 	mask_arr = np.copy(gtv_arr)
+	healthy_tissue_mean = mask_arr[np.where(mask_arr<threshold and mask_arr>0)].mean()
+	healthy_tissue_variance = mask_arr[np.where(mask_arr<threshold and mask_arr>0)].mean()
 	mask_arr[np.where(mask_arr<threshold)] = 0
 	for i in range(np.shape(mask_arr)[2]):
 		scan_slice = scipy.ndimage.binary_fill_holes(mask_arr[:,:,i])
-		scan_slice = skimage.morphology.remove_small_objects(scan_slice.astype(bool), min_size=10, connectivity=1, in_place=False)
-		edges = feature.canny(scan_slice,sigma=2)
+		scan_slice = skimage.morphology.remove_small_objects(scan_slice.astype(bool), min_size=small_obj_size, connectivity=1, in_place=False)
+		edges = feature.canny(scan_slice,sigma=sigma)
 		mask = skimage.filters.rank.maximum(edges,skimage.morphology.disk(radius_of_mask))
 		mask_arr[:,:,i] = mask
 		
-	return mask_arr
+	return mask_arr,healthy_tissue_mean
+
+# gets the values of healthy tissue mean and standard devation around the tumour
+def get_healthy_tissue_vals(gtv_arr,threshold = 500):
+	return gtv_arr[np.where(mask_arr<threshold and mask_arr>0)].mean(),gtv_arr[np.where(mask_arr<threshold and mask_arr>0)].var()
 	
+
+
 def show_slice(slice_obj):
     fig,axes = plt.subplots(1)
     axes.imshow(slice_obj, cmap='gray',origin='lower')
